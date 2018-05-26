d629f322cf29b5113afb9bbb921ea0c1769995b5
sumit kumar sengupta <holasumit@gmail.com>
2018-05-26 11:38:19 +0530

    feat makegrid function in js

--- design.js
+++ design.js
@@ -1,13 +1,4 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
 // When size is submitted by the user, call makeGrid()
 var submitQuery;
 submitQuery = $('input[type="submit"]')
@@ -15,3 +6,33 @@ submitQuery.click(function(event) {
   event.preventDefault();
   makeGrid();
 });
+// Function to make grid like structure.
+	function makeGrid() {
+	console.log("Executing makeGrid") 
+// Declaring variables so that we can put data in them, which will make the grid.
+	var gridAxisY, gridAxisX, gridRow, gridBox, canvas;
+// Assigning values to the variables
+	canvas = $('#pixelCanvas');
+	gridAxisY = $('#inputHeight').val();
+	gridAxisX = $('#inputWidth').val();
+// Declaring function	
+	canvas.children().remove()
+// for loop to make Row	
+	for (y = 0; y < gridAxisY; y++) {
+	canvas.append('<tr></tr>');
+	}
+	gridRow = $('tr');
+// for loop to make box
+	for (x = 0; x < gridAxisX; x++) {
+	gridRow.append('<td></td>');
+	} 
+	gridBox = canvas.find('td');
+// When td is clicked by the user, change color of td
+	gridBox.click(function() {
+// Select color input
+		console.log("Executing changeColor");
+		var fillColor;
+		fillColor = $("#colorPicker").val();
+		$(this).attr('bgcolor', fillColor);
+	});
+}
